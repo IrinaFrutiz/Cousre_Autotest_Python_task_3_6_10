f434d178c038ef1a32faee8d8e7e9a604d7441e1
irina <irinafrutiz@gmail.com>
2023-04-23 17:37:58 +0200

    home task files

---
+++ test_items.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_chech_add_book_to_busket (browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    browser.get(link)
+    add_to_busket_button = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="add_to_basket_form"]/button'))
+    )
+
+    if language=="ru":
+        assert 'Добавить в корзину'==add_to_busket_button.text, \
+        f'{add_to_busket_button.text} is not Добавить в корзину'
+
+    if language=="es":
+        assert 'Añadir al carrito'==add_to_busket_button.text, \
+        f'{add_to_busket_button.text} is not Añadir al carrito'
+
+    if language=="en - gb":
+        assert 'Add to basket'==add_to_busket_button.text, \
+        f'{add_to_busket_button.text} is not Add to basket'
