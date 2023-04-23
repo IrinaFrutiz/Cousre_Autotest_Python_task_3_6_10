333a8275f33a16e1180d856ccb8dc14a3c38aaf4
irina <irinafrutiz@gmail.com>
2023-04-23 17:48:00 +0200

    correction to naming

--- test_items.py
+++ test_items.py
@@ -2,13 +2,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_chech_add_book_to_busket (browser, language):
+def test_chech_add_book_to_basket (browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
     add_to_busket_button = WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@id="add_to_basket_form"]/button'))
     )
-
+    assert
     if language=="ru":
         assert 'Добавить в корзину'==add_to_busket_button.text, \
         f'{add_to_busket_button.text} is not Добавить в корзину'
@@ -20,3 +20,4 @@ def test_chech_add_book_to_busket (browser, language):
     if language=="en - gb":
         assert 'Add to basket'==add_to_busket_button.text, \
         f'{add_to_busket_button.text} is not Add to basket'
+
