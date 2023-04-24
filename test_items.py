d67affb294f673d878f74dd0b71ba1c19fc26046
irina <irinafrutiz@gmail.com>
2023-04-24 11:52:08 +0200

    correction to assert

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -5,19 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_chech_add_book_to_basket (browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    add_to_busket_button = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '//*[@id="add_to_basket_form"]/button'))
-    )
-    assert
-    if language=="ru":
-        assert 'Добавить в корзину'==add_to_busket_button.text, \
-        f'{add_to_busket_button.text} is not Добавить в корзину'
+    try:
+        add_to_basket_button = WebDriverWait(browser, 10).until(
+         EC.element_to_be_clickable((By.XPATH, '//*[@id="add_to_basket_form"]/button')))
+        assert add_to_basket_button.is_displayed()
+    except:
+        pytest.fail("Can't find button Add to basket")
 
-    if language=="es":
-        assert 'Añadir al carrito'==add_to_busket_button.text, \
-        f'{add_to_busket_button.text} is not Añadir al carrito'
-
-    if language=="en - gb":
-        assert 'Add to basket'==add_to_busket_button.text, \
-        f'{add_to_busket_button.text} is not Add to basket'
 
